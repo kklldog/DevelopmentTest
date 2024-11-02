4db8f1846e9328f98f7c86009c9f46be1d293eac
agile.zhou <kklldog@live.cn>
2024-11-03 00:41:01 +0800

    Add project files.

---
+++ MockSerivce.cs
@@ -0,0 +1,15 @@
+﻿namespace DevelopmentTest
+{
+    public interface Interface1
+    {
+        void Method1();
+    }
+
+    public class MockSerivce
+    {
+        public MockSerivce(Interface1 interface1)
+        {
+
+        }
+    }
+}
